2ab43ce3d2240bdd6ebd5fab3c4ca6a0775ff899
김덕곤(Deokgon Kim) 대리 서로커뮤니케이션 <deokgon1.kim@doosan.com>
2024-03-12 10:31:29 +0900

    Initial commit

---
+++ src/main/java/com/gon/GonApplication.java
@@ -0,0 +1,13 @@
+package com.gon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gon/GonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
